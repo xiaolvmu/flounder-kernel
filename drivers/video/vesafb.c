5c8a49267ee8b10c83da91bcfbcc7fd772f34fe8
Joe Perches <joe@perches.com>
2013-09-19 18:35:55 -0700

    framebuffer: Use fb_<level>
    Neaten and shorten the code using the new fb_<level> macros.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

--- drivers/video/vesafb.c
+++ drivers/video/vesafb.c
@@ -484,8 +484,7 @@ static int __init vesafb_probe(struct platform_device *dev)
 		fb_dealloc_cmap(&info->cmap);
 		goto err;
 	}
-	printk(KERN_INFO "fb%d: %s frame buffer device\n",
-	       info->node, info->fix.id);
+	fb_info(info, "%s frame buffer device\n", info->fix.id);
 	return 0;
 err:
 	if (info->screen_base)
