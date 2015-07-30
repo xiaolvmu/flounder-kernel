29805ab492ec44ed135c59c68d100f0d6146da2b
xiaolvmu <i@xiaolvmu.me>
2015-07-30 08:28:28 +0800

    Backport: TCP Auto Corking

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -778,6 +778,15 @@ static struct ctl_table ipv4_table[] = {
 		.proc_handler   = proc_tcp_default_init_rwnd
 	},
 	{
+		.procname	= "tcp_autocorking",
+		.data		= &sysctl_tcp_autocorking,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
+	},
+	{
 		.procname	= "udp_mem",
 		.data		= &sysctl_udp_mem,
 		.maxlen		= sizeof(sysctl_udp_mem),
