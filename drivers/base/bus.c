cfd9e24c5449a2ea45385fe3859f2f32f27ad874
xiaolvmu <i@xiaolvmu.me>
2015-10-01 21:39:23 -0700

    Revert "sysfs.h: remove attr_name() macro"
    This reverts commit 0a39922c9405c997a1fa5ace974f3e0e948bf046.

--- drivers/base/bus.c
+++ drivers/base/bus.c
@@ -460,7 +460,7 @@ static int device_add_attrs(struct bus_type *bus, struct device *dev)
 	if (!bus->dev_attrs)
 		return 0;
 
-	for (i = 0; bus->dev_attrs[i].attr.name; i++) {
+	for (i = 0; attr_name(bus->dev_attrs[i]); i++) {
 		error = device_create_file(dev, &bus->dev_attrs[i]);
 		if (error) {
 			while (--i >= 0)
@@ -476,7 +476,7 @@ static void device_remove_attrs(struct bus_type *bus, struct device *dev)
 	int i;
 
 	if (bus->dev_attrs) {
-		for (i = 0; bus->dev_attrs[i].attr.name; i++)
+		for (i = 0; attr_name(bus->dev_attrs[i]); i++)
 			device_remove_file(dev, &bus->dev_attrs[i]);
 	}
 }
@@ -590,7 +590,7 @@ static int driver_add_attrs(struct bus_type *bus, struct device_driver *drv)
 	int i;
 
 	if (bus->drv_attrs) {
-		for (i = 0; bus->drv_attrs[i].attr.name; i++) {
+		for (i = 0; attr_name(bus->drv_attrs[i]); i++) {
 			error = driver_create_file(drv, &bus->drv_attrs[i]);
 			if (error)
 				goto err;
@@ -610,7 +610,7 @@ static void driver_remove_attrs(struct bus_type *bus,
 	int i;
 
 	if (bus->drv_attrs) {
-		for (i = 0; bus->drv_attrs[i].attr.name; i++)
+		for (i = 0; attr_name(bus->drv_attrs[i]); i++)
 			driver_remove_file(drv, &bus->drv_attrs[i]);
 	}
 }
@@ -846,7 +846,7 @@ static int bus_add_attrs(struct bus_type *bus)
 	int i;
 
 	if (bus->bus_attrs) {
-		for (i = 0; bus->bus_attrs[i].attr.name; i++) {
+		for (i = 0; attr_name(bus->bus_attrs[i]); i++) {
 			error = bus_create_file(bus, &bus->bus_attrs[i]);
 			if (error)
 				goto err;
@@ -865,7 +865,7 @@ static void bus_remove_attrs(struct bus_type *bus)
 	int i;
 
 	if (bus->bus_attrs) {
-		for (i = 0; bus->bus_attrs[i].attr.name; i++)
+		for (i = 0; attr_name(bus->bus_attrs[i]); i++)
 			bus_remove_file(bus, &bus->bus_attrs[i]);
 	}
 }
