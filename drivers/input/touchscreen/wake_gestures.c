29c4ab6c68a9860962f3a7248ed94b6128fcd0a1
xiaolvmu <i@xiaolvmu.me>
2015-12-12 13:22:43 +0800

    wake_gestures: disabled by default

--- drivers/input/touchscreen/wake_gestures.c
+++ drivers/input/touchscreen/wake_gestures.c
@@ -37,8 +37,8 @@
 /* Tuneables */
 #define WG_DEBUG		0
 #define WG_DEFAULT		0
-#define S2W_DEFAULT		15
-#define S2S_DEFAULT		1
+#define S2W_DEFAULT		0
+#define S2S_DEFAULT		0
 #define WG_PWRKEY_DUR           60
 
 /* flounder */
